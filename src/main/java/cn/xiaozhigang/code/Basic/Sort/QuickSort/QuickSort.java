ca069fd5cdf74f98dce944009767aebc3baa9797
xiaozhigang1 <xiaozhigang1@jd.com>
2018-08-19 10:44:55 +0800

    贝壳笔试;头条帮忙笔试;剑指offer部分题

--- src/main/java/cn/xiaozhigang/code/Basic/Sort/QuickSort/QuickSort.java
+++ src/main/java/cn/xiaozhigang/code/Basic/Sort/QuickSort/QuickSort.java
@@ -7,7 +7,7 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] test = {4,3,10,1,100,-1};
         int[] test2 = {10,9,8,7,6};
-        int[] test3 = {1,2,3,4,5};
+        int[] test3 = {4,3};
         quickSort(test);
         quickSort(test2);
         quickSort(test3);
@@ -18,35 +18,73 @@ public class QuickSort {
     }
 
     public static void quickSort(int[] arr) {
-        if(arr == null || arr.length == 0) return;
-        quickSort(arr,0,arr.length - 1);
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        quicksort(arr,0,arr.length - 1);
     }
 
-    private static void quickSort(int [] arr,int i,int j) {
-        int index = Partition(arr,i,j);
-        if(index > i) quickSort(arr,i,index - 1);
-        if(index < j) quickSort(arr,index + 1,j);
+    private static void quicksort(int[] arr,int start,int end) {
+        if (start < end) {
+            int index = partition(arr, start, end);
+            quicksort(arr, start, index - 1);
+            quicksort(arr, index + 1, end);
+        }
     }
 
-    public static int Partition(int[] arr,int start,int end) {
+    private static int partition(int[] arr,int start,int end) {
+        int privot = arr[start];
+        int small = start - 1;
+        swap(arr, start, end);
         int index = start;
-        int small = index - 1;
-        swap(arr,index,end);
-        for(index = start; index < end; index++) {
-            if(arr[index] < arr[end]) {
+        for(;index < end; index++) {
+            if (arr[index] < privot) {
                 small++;
-                if(small != index)
-                    swap(arr,small,index);
+                swap(arr, small, index);
             }
         }
         small++;
-        swap(arr,small,end);
+        swap(arr, small, end);
         return small;
     }
 
-    private static void swap(int [] arr,int i,int j) {
+    private static void swap(int[] arr,int i,int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
+
+//    public static void quickSort(int[] arr) {
+//        if(arr == null || arr.length == 0) return;
+//        quickSort(arr,0,arr.length - 1);
+//    }
+//
+//    private static void quickSort(int [] arr,int i,int j) {
+//        int index = Partition(arr,i,j);
+//        if(index > i) quickSort(arr,i,index - 1);
+//        if(index < j) quickSort(arr,index + 1,j);
+//    }
+//
+//    public static int Partition(int[] arr,int start,int end) {
+//        int index = start;
+//        int small = index - 1;
+//        swap(arr,index,end);
+//        for(index = start; index < end; index++) {
+//            if(arr[index] < arr[end]) {
+//                small++;
+//                if(small != index)
+//                    swap(arr,small,index);
+//            }
+//        }
+//        small++;
+//        swap(arr,small,end);
+//        return small;
+//    }
+//
+//    private static void swap(int [] arr,int i,int j) {
+//        int tmp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = tmp;
+//    }
 }
